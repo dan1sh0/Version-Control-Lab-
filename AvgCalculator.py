e018a85841d7543d13e39eb252e7bb364811cd3f
Danish Ghumman <114195774+dan1sh0@users.noreply.github.com>
2023-02-18 00:26:28 +0000

    done!

--- AvgCalculator.py
+++ AvgCalculator.py
@@ -1,7 +1,15 @@
+numbers = []
 while True: 
    # enter as many numbers as you want 
-    num = input("Enter a number: ")
-    numbers = []
-    numbers.append(num)
+   num = input("Enter a number(or enter 'done' to finish): ")
+   if num == 'done':
+      break
+   else:
+      numbers.append(float(num))
 
-
+if numbers:
+    average = sum(numbers) / len(numbers)
+    print("The average of the numbers is:", average)
+else:
+    print("No numbers entered.")
+   
